9493226765a1c542b95dff74bbffe1b0e0de4444
Nayar Joolfoo <nayar@joolfoo.com>
2014-04-27 09:11:39 -0400

    finished program

--- main.py
+++ main.py
@@ -5,5 +5,29 @@ sequence_file = open('sequence.dat', 'r') # Reading sequence.dat and put into va
 for sequence in sequence_file:
   sequences_array.append(sequence) # Put each line in the array
   
-for l in sequences_array:
-  print l
+total = []
+
+for s in sequences_array: 
+  count_a = 0
+  count_c = 0
+  count_t = 0
+  count_g = 0
+  for neucleotide in s:
+    if (neucleotide ==  'A'):
+      #print("A")
+      count_a = count_a + 1
+    elif (neucleotide == 'C'):
+      #print("C")
+      count_c = count_c + 1
+
+    elif (neucleotide == 'T'):
+      #print("T")
+      count_t = count_t + 1
+    elif (neucleotide == 'G'):
+      #print("G")
+      count_g = count_g + 1
+  total_length = count_a + count_c + count_t + count_g
+  total.append([count_a,count_c,count_t,count_g,total_length])
+  print "%d %d %d %d %d" % (count_a,count_c,count_t,count_g,total_length)
+  
+      
