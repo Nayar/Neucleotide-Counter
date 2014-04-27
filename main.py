a74b5fdc81a4b6bf264284b63eaefe148a3c8797
Nayar Joolfoo <nayar@joolfoo.com>
2014-04-27 08:47:24 -0400

    reading from file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-x = 4
-y = 5
-print x + y
+sequences_array = []
+
+sequence_file = open('sequence.dat', 'r')
+
+for sequence in sequence_file:
+  sequences_array.append(sequence)
+  
+for l in sequences_array:
+  print l
