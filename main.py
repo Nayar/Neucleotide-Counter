1be543a132c9e8dac775dfbed966db7dbb2ff5a3
Nayar Joolfoo <nayar@joolfoo.com>
2014-04-27 08:54:22 -0400

    adding comment

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-sequences_array = []
+sequences_array = [] # Initialising 1D array to store sequences
 
-sequence_file = open('sequence.dat', 'r')
+sequence_file = open('sequence.dat', 'r') # Reading sequence.dat and put into variable
 
 for sequence in sequence_file:
-  sequences_array.append(sequence)
+  sequences_array.append(sequence) # Put each line in the array
   
 for l in sequences_array:
   print l
