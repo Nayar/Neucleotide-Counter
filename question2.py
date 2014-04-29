55fc189617ca71f1b86e4740f11ebc198fd2baa2
Nayar Joolfoo <nayar@joolfoo.com>
2014-04-29 23:11:25 +0400

    calcualtion of mass

--- question2.py
+++ question2.py
@@ -1,3 +1,8 @@
+mass_a = 135
+mass_c = 111
+mass_t = 151
+mass_g = 126
+
 sequences_array = [] # Initialising 1D array to store sequences
 
 sequence_file = open('sequence.dat', 'r') # Reading sequence.dat and put into variable
@@ -29,9 +34,14 @@ for s in sequences_array:
   total_length = count_a + count_c + count_t + count_g
   total.append([count_a,count_c,count_t,count_g,total_length])
   
-for sequence in sequences_array:
-  print (sequence)
+def calc_total_sequence_mass(no_of_a, no_of_c, no_of_t, no_of_g):
+  return no_of_a * mass_a + no_of_c * mass_c + no_of_t * mass_t + no_of_g * mass_g
+
+for i in range(len(sequences_array)):
+  print(sequences_array[i])
+  print("Total Mass: %d" % calc_total_sequence_mass(total[i][0],total[i][1],total[i][2],total[i][3]))
+  print("---------------------")
   
-for t in total:
-  print ("%d %d %d %d %d" % (t[0],t[1],t[2],t[3],t[4]))
-      
+  
+
+
